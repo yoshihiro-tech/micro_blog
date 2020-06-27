313f55e29fc31427f84418f347f308e128cfecb6
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-06-27 18:34:00 +0900

    Create a Model for comments and dates.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+
+    #autoID
+    content = models.CharField(max_length=140)
+    posted_date = models.DateTimeField(auto_now_add=True)
