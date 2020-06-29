55d274dd2f0bd9dd0357cba98019aa9c410dc82d
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-06-29 23:24:55 +0900

    create updateView

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-from django.views.generic import ListView
-from django.views.generic import DetailView
+
+from django.urls import reverse_lazy
+
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .models import Blog
-
 
 class BlogListView(ListView):
     model = Blog
@@ -11,3 +11,16 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Blog
 
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ["content"]
+    success_url = reverse_lazy("index")
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ["content"]
+
+    def get_success_url(self):
+        blog_pk = self.kwargs['pk']
+        url = reverse_lazy("detail", kwargs={"pk": blog_pk})
+        return url
--- micro_blog/urls.py
+++ micro_blog/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import BlogListView,BlogDetailView
+from blog.views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
 
 urlpatterns = [
     #path('<URL>',views(関数),ニックネーム)
     path('',BlogListView.as_view(),name="index"),
-    path('<int:pk>',BlogDetailView.as_view(),name="detail"),
+    path('<int:pk>', BlogDetailView.as_view(), name="detail"),
+    path('create', BlogCreateView.as_view(), name="create"),
+    path('<int:pk>/update', BlogUpdateView.as_view(), name="update"),
     path('admin/', admin.site.urls),
 ]
