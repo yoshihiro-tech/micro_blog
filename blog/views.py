f02da0bad90f9ebdd66d271bc2c16e66b66b6757
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-06-30 21:02:57 +0900

    Post a link to each page

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from django.views.generic import ListView
 from django.views.generic import DetailView
 from django.views.generic import CreateView
 from django.views.generic import UpdateView
+from django.views.generic import DeleteView
 
 from .models import Blog
 from .forms import BlogForm
@@ -28,3 +29,7 @@ class BlogUpdateView(UpdateView):
         blog_pk = self.kwargs['pk']
         url = reverse_lazy("detail", kwargs={"pk": blog_pk})
         return url
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy("index")
--- micro_blog/urls.py
+++ micro_blog/urls.py
@@ -5,12 +5,13 @@ from blog.views import BlogListView
 from blog.views import BlogDetailView
 from blog.views import BlogCreateView
 from blog.views import BlogUpdateView
+from blog.views import BlogDeleteView
 
 urlpatterns = [
-    # path('<URL>',views(関数),ニックネーム)
-    path('',BlogListView.as_view(),name="index"),
+    path('', BlogListView.as_view(), name="index"),
     path('<int:pk>', BlogDetailView.as_view(), name="detail"),
     path('create', BlogCreateView.as_view(), name="create"),
     path('<int:pk>/update', BlogUpdateView.as_view(), name="update"),
+    path('<int:pk>/delete', BlogDeleteView.as_view(), name="delete"),
     path('admin/', admin.site.urls),
 ]
