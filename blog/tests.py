c7f4fda164531bf6844bf9ec5cc4fa53c4dc1112
yoshihiro.uda <yoshihiro7319@yahoo.co.jp>
2020-07-01 19:34:40 +0900

    add test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class BlogTestCase(TestCase):
+
+    def setUp(self):
+        self.c = Client()
+
+    def test_index_access(self):
+        res = self.c.get('/')
+        self.assertEqual(200, res.status_code)
+
+    def test_fail_access(self):
+        res = self.c.get('/unknown')
+        self.assertEqual(404, res.status_code)
